b4ff22aed544108da256589433bebdfddd00e103
Karen Dayana Suarez Ramos <karensuarez1120@gmail.com>
2025-05-21 20:08:03 -0500

    Se agregan estilos

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -15,7 +15,8 @@ import { AuthService } from './../../../core/services/auth.service';
       <input [(ngModel)]="password" type="password" placeholder="Contraseña" />
       <button (click)="login()">Ingresar</button>
     </div>
-  `
+  `,
+  styleUrls: ['./login.component.css'] // ¡Agrega esta línea!
 })
 export class LoginComponent {
   username = '';
@@ -24,28 +25,28 @@ export class LoginComponent {
   constructor(private auth: AuthService, private router: Router) {}
 
   login() {
-  const success = this.auth.login(this.username, this.password);
-  if (success) {
-    const user = this.auth.getUser();
-    if (user) {
-      switch (user.role) {
-        case 'admin':
-          this.router.navigate(['/dashboard/admin']);
-          break;
-        case 'tutor':
-          this.router.navigate(['/dashboard/docente']);
-          break;
-        case 'student':
-          this.router.navigate(['/dashboard/estudiante']);
-          break;
-        default:
-          alert('Rol desconocido');
+    const success = this.auth.login(this.username, this.password);
+    if (success) {
+      const user = this.auth.getUser();
+      if (user) {
+        switch (user.role) {
+          case 'admin':
+            this.router.navigate(['/dashboard/admin']);
+            break;
+          case 'tutor':
+            this.router.navigate(['/dashboard/docente']);
+            break;
+          case 'student':
+            this.router.navigate(['/dashboard/estudiante']);
+            break;
+          default:
+            alert('Rol desconocido');
+        }
+      } else {
+        alert('No se pudo obtener el usuario');
       }
     } else {
-      alert('No se pudo obtener el usuario');
+      alert('Credenciales incorrectas');
     }
-  } else {
-    alert('Credenciales incorrectas');
   }
-}
 }
